332c410a4eb9201a45c5a181e042ee5c6c675500
Thomas Drège <thomasdrege1@gmail.com>
2019-04-20 14:59:01 +0200

    add child 2

--- app/src/main/java/com/ziggy/kdo/ui/fragment/children/AddChildFragment.kt
+++ app/src/main/java/com/ziggy/kdo/ui/fragment/children/AddChildFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.ziggy.kdo.R
+import com.ziggy.kdo.databinding.FragmentAddChildBinding
 import com.ziggy.kdo.ui.base.BaseFragment
 
 /**
@@ -15,13 +18,36 @@ import com.ziggy.kdo.ui.base.BaseFragment
  */
 class AddChildFragment : BaseFragment() {
 
+    private lateinit var mChildViewModel: ChildViewModel
+
+    private lateinit var mChildFragmentBinding: FragmentAddChildBinding
+
+    private var mView: View? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_add_child, container, false)
+        mView?.let {
+            return mView
+        } ?: kotlin.run {
+            mChildFragmentBinding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_add_child, container, false)
+            mView = mChildFragmentBinding.root
+        }
+
+        return mView
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.also { activity ->
+            mChildViewModel = ViewModelProviders.of(activity, mViewModeFactory).get(ChildViewModel::class.java)
+
+            mChildFragmentBinding.childViewModel = mChildViewModel
+            mChildFragmentBinding.lifecycleOwner =activity
+        }
+    }
 
 }
