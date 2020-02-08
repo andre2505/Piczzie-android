14a76d5557beff3b167dcd80835f522278bb1f5f
andre2505 <thomasdrege1@gmail.com>
2020-02-08 16:12:58 +0100

    Information user edit 5

--- app/src/main/java/com/ziggy/kdo/ui/fragment/settings/edit_profile/user_information/UserInformationFragment.kt
+++ app/src/main/java/com/ziggy/kdo/ui/fragment/settings/edit_profile/user_information/UserInformationFragment.kt
@@ -1,6 +1,7 @@
 package com.ziggy.kdo.ui.fragment.settings.edit_profile.user_information
 
 
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -17,18 +18,32 @@ import androidx.navigation.fragment.findNavController
 import com.ziggy.kdo.R
 import com.ziggy.kdo.databinding.FragmentUserInformationBinding
 import com.ziggy.kdo.enums.Error
+import com.ziggy.kdo.generated.callback.OnClickListener
 import com.ziggy.kdo.ui.base.BaseFragment
 import com.ziggy.kdo.ui.fragment.profile.ProfileViewModel
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class UserInformationFragment : BaseFragment() {
+class UserInformationFragment : BaseFragment(), View.OnClickListener {
+
+    private val cal = Calendar.getInstance()
 
     private lateinit var mProfileViewModel: ProfileViewModel
 
     private lateinit var mUserInformationBinding: FragmentUserInformationBinding
+
+    private val dateSetListener =
+        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            mProfileViewModel.mUser.value?.birthday = cal.time
+            mProfileViewModel.mUser.value = mProfileViewModel.mUser.value
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,12 +94,26 @@ class UserInformationFragment : BaseFragment() {
         mUserInformationBinding.profilViewModel = mProfileViewModel
         mUserInformationBinding.lifecycleOwner = this@UserInformationFragment
 
-        mUserInformationBinding.registerBirthday.setOnClickListener {
-
-        }
+        mUserInformationBinding.registerBirthday.setOnClickListener(this@UserInformationFragment)
 
         return mUserInformationBinding.root
     }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.register_birthday -> {
+                DatePickerDialog(
+                    activity!!,
+                    R.style.SpinnerDatePickerStyle,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+    }
+
 
     private fun showAlertDialogError() {
 
